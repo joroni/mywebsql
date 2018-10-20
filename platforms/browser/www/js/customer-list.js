f196cdf178a62cbd4b1256a7e70c7ca622e49306
joroni <jraymund.niconi@gmail.com>
2018-10-21 02:17:11 +0800

    ready to test on device

--- platforms/browser/www/js/customer-list.js
+++ platforms/browser/www/js/customer-list.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     init();
 
-    listarIntegrantes();
+    memberList();
 
     console.info("Carga Complete...");
 
@@ -15,19 +15,19 @@ $(document).ready(function () {
     ////////////////////////////////////////////////////////////////////
     $('#submit').click(function () {
         var txtId = $('#txt-id');
-        var txtNombres = $('#fname');
-        var txtApellidos = $('#lname');
+        var txtFname = $('#fname');
+        var txtLname = $('#lname');
         var input = $('input');
 
-        if (txtNombres.val() === '' || txtApellidos.val() === '') {
+        if (txtFname.val() === '' || txtLname.val() === '') {
             alert("All fields are Required...");
             return;
         }
 
         var integrante = Object();
 
-        integrante.nombres = txtNombres.val();
-        integrante.apellidos = txtApellidos.val();
+        integrante.fname = txtFname.val();
+        integrante.lname = txtLname.val();
 
         if (txtId.val() === '') { //Lo guarda
             integrante.id = new Date().getTime();
@@ -37,23 +37,36 @@ $(document).ready(function () {
             actualizarIntegrante(integrante);
         }
 
-        listarIntegrantes();
+        memberList();
 
-        txtNombres.val(null);
-        txtApellidos.val(null);
+        txtFname.val(null);
+        txtLname.val(null);
         txtId.val(null);
     });
 
+
+    
+    $('#li-integrantes').on("click", ".btn-user-info", function () {
+        var idMember = $(this).data("id");
+        consultarIntegrante(idMember);
+        $("label").addClass("active");
+        //$("#modal-Title").html("View Customer");
+
+    });
+
+
     $('#li-integrantes').on("click", ".btn-editar", function () {
-        var idIntegrante = $(this).data("id");
-        consultarIntegrante(idIntegrante);
-        $("label").toggleClass("hidden");
+        var idMember = $(this).data("id");
+        consultarIntegrante(idMember);
+        $("label").addClass("active");
+        $("#modal-Title").html("Edit Customer");
+
     });
 
     $('#li-integrantes').on("click", ".btn-eliminar", function () {
-        var idIntegrante = $(this).data("id");
-        eliminarIntegrante(idIntegrante);
-        listarIntegrantes();
+        var idMember = $(this).data("id");
+        removeMember(idMember);
+        memberList();
     });
 
 });
@@ -67,38 +80,58 @@ function init() {
     }, error, exito);
 }
 
-function listarIntegrantes() {
+function memberList() {
     db.readTransaction(function (t) {
         t.executeSql('SELECT ID, NOMBRES, APELLIDOS FROM INTEGRANTE', [], function (t, rs) {
             if (rs.rows.length > 0) {
                 var lisHtml = '';
-               
+
                 for (var i = 0; i < rs.rows.length; i++) {
                     var integrante = rs.rows.item(i);
                     var id = integrante.ID;
 
 
-                  
-                 lisHtml += '<a class="list-group-item list-group-item-action"><div class="media">'+
-                 '<div href="#" class="icon-btn toggleTools" id="toggleTools'+ integrante.ID+'"><i class="material-icons fa-2x">more_vert</i></div>'+
-                            '<img src="img/faces/avatar.jpg"  class="mr-3 img-circle" width="64" alt="Sample Image">'+
-                 
-                   '<div class="media-body"><h5 class="mt-0">' + integrante.NOMBRES + ' ' + integrante.APELLIDOS +'</h5><p> ' + integrante.ID +'</p><div class="showData tools" id="tools'+ integrante.ID+'">'+
-                        '<button class="btn btn-info btn-editar" data-toggle="modal" data-target="#modalCart" type="button" data-id="' + id + '"><i class="material-icons">edit</i></button>' +
-                        '<button class="btn btn-info btn-eliminar" type="button" data-id="' + id + '"><i class="material-icons">close</i></Eliminar></div></div></div>';
-               
-                       
-                    }
 
-               
+                    lisHtml += '<a class="list-group-item list-group-item-action"><div class="media">' +
+                        '<div href="#"  data-toggle="class" data-target="#tools' + integrante.ID + '" class="icon-btn toggleTools" id="toggleTools' + integrante.ID + '"><i class="material-icons fa-2x">more_vert</i></div>' +
+                        '<img src="img/user.svg"  class="mr-3  btn-user-info img-circle" width="64" alt="Sample Image">' +
+
+                        '<div class="media-body"><h5 class="mt-0">' + integrante.NOMBRES + ' ' + integrante.APELLIDOS + '</h5><p> ' + integrante.ID + '</p><div id="tools' + integrante.ID + '" class="showData edittool">' +
+                        '<button class="btn btn-info btn-editar" data-toggle="modal" data-target="#modalCart" type="button" data-id="' + id + '"><i class="material-icons">edit</i></button>' +
+                        '<button class="btn btn-info btn-eliminar" type="button" data-id="' + id + '"><i class="material-icons">delete</i></Eliminar></div></div></div>';
+
+
+                }
+
+
                 $('#li-integrantes').html(lisHtml);
 
-                $(function() {
-                    
-                    $('.icon-btn').on('click', function() {
-                       $('.showData').toggle();    
+               /* $(function () {
+                    $('.icon-btn').on('click', function () {
+                        $('.showData').toggle();
                     });
-                   });
+                });*/
+
+
+                $('[data-toggle="class"]').click(function(){
+                    var $target = $($(this).data('target'));
+                    var classes = $(this).data('classes');
+                    $target.toggleClass(classes);
+                    return false;
+                });
+                
+
+
+
+                $("#btnNew").click(function () {
+                    console.log("reset fields");
+                    $('#dynamic-form')[0].reset();
+                    $('#txt-id').val("");
+                      $("#modal-Title").html("Add Customer");
+
+                })
+
+
 
             }
 
@@ -108,24 +141,20 @@ function listarIntegrantes() {
 
 function guardarIntegrante(integrante) {
     db.transaction(function (tx) {
-        tx.executeSql('INSERT INTO INTEGRANTE(ID, NOMBRES, APELLIDOS) VALUES(?, ?, ?)', [integrante.id, integrante.nombres, integrante.apellidos]);
+        tx.executeSql('INSERT INTO INTEGRANTE(ID, NOMBRES, APELLIDOS) VALUES(?, ?, ?)', [integrante.id, integrante.fname, integrante.lname]);
     }, error, function () {
         alert("Item Saved.");
-        $(".close").trigger()
-      /*  $("#modalCart").css("display","none");
-        $("#modalCart, .modal-backdrop").removeClass("show");
-        $("body").removeClass("modal-open");*/
+        $(".close").trigger();
     });
 }
 
-function consultarIntegrante(idIntegrante) {
+function consultarIntegrante(idMember) {
     db.readTransaction(function (t) {
-        t.executeSql('SELECT ID, NOMBRES, APELLIDOS FROM INTEGRANTE WHERE ID = ?', [idIntegrante], function (t, rs) {
+        t.executeSql('SELECT ID, NOMBRES, APELLIDOS FROM INTEGRANTE WHERE ID = ?', [idMember], function (t, rs) {
             if (rs.rows.length > 0) {
                 var integrante = new Object();
-                integrante.nombres = rs.rows.item(0).NOMBRES;
-                integrante.apellidos = rs.rows.item(0).APELLIDOS;
-
+                integrante.fname = rs.rows.item(0).NOMBRES;
+                integrante.lname = rs.rows.item(0).APELLIDOS;
                 $('#txt-id').val(rs.rows.item(0).ID);
                 $('#fname').val(rs.rows.item(0).NOMBRES);
                 $('#lname').val(rs.rows.item(0).APELLIDOS);
@@ -137,19 +166,21 @@ function consultarIntegrante(idIntegrante) {
 
 function actualizarIntegrante(integrante) {
     db.transaction(function (tx) {
-        tx.executeSql('UPDATE INTEGRANTE SET NOMBRES = ?, APELLIDOS = ? WHERE ID = ?', [integrante.nombres, integrante.apellidos, integrante.id]);
+        tx.executeSql('UPDATE INTEGRANTE SET NOMBRES = ?, APELLIDOS = ? WHERE ID = ?', [integrante.fname, integrante.lname, integrante.id]);
     }, error, function () {
         alert("El integrante ha sido actualizado con éxito");
     });
 }
 
-function eliminarIntegrante(idIntegrante) {
+function removeMember(idMember) {
     db.transaction(function (tx) {
-        tx.executeSql('DELETE FROM INTEGRANTE WHERE ID = ?', [idIntegrante]);
+        tx.executeSql('DELETE FROM INTEGRANTE WHERE ID = ?', [idMember]);
     }, error, function () {
         alert("El integrante ha sido eliminado con éxito");
     });
 }
+
+
 
 
 ////////////////////////////////////////////////////////////////////
